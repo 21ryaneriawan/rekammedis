ddbbd74266a01003f451bca46744812a87026cbf
21ryaneriawan <21ryaneriawan@gmail.com>
2020-06-13 16:31:45 +0700

    tugas akhir

--- application/views/dokter/tambah_dokter.php
+++ application/views/dokter/tambah_dokter.php
@@ -47,8 +47,6 @@
 
 <!-- =============================================== -->
 
-
-
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
