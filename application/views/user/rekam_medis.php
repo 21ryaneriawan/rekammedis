ec054e6f950b194e5e87a5919234f7fac90aaadf
21ryaneriawan <21ryaneriawan@gmail.com>
2020-06-24 22:44:51 +0700

    no message

--- application/views/user/rekam_medis.php
+++ application/views/user/rekam_medis.php
@@ -123,7 +123,7 @@
                                         <?php } ?>
                                     </table>
                                     <br>
-                                    <table id="example1" class="table table-bordered table-striped">
+                                    <table id="example1" class="table table-bordered table-striped" width='100%'>
                                         <thead>
                                             <tr>
                                                 <td width=60px>Tanggal</td>
